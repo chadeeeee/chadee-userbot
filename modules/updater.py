7d5e908a218e94af44bf061dc1384148a414bbd9
deadboizxc <deadboi.zxc@gmail.com>
2023-10-16 00:59:27 +0200

    added updater.py in modules

---
+++ modules/updater.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import subprocess
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from utils.misc import modules_help, prefix, requirements_list
+from utils.db import db
+from utils.scripts import format_exc, restart
+
+
+@Client.on_message(filters.command("restart", prefix) & filters.me)
+async def restart_cmd(_, message: Message):
+    db.set(
+        "core.updater",
+        "restart_info",
+        {
+            "type": "restart",
+            "chat_id": message.chat.id,
+            "message_id": message.id,
+        },
+    )
+
+    await message.edit("<b>Restarting...</b>")
+    restart()
+
+
+@Client.on_message(filters.command("update", prefix) & filters.me)
+async def update(_, message: Message):
+    db.set(
+        "core.updater",
+        "restart_info",
+        {
+            "type": "update",
+            "chat_id": message.chat.id,
+            "message_id": message.id,
+        },
+    )
+
+    await message.edit("<b>Updating...</b>")
+    try:
+        subprocess.run([sys.executable, "-m", "pip", "install", "-U", "pip"])
+        subprocess.run(["git", "pull"])
+        subprocess.run(
+            [
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "-U",
+                "-r",
+                "requirements.txt",
+            ]
+        )
+        subprocess.run(
+            [sys.executable, "-m", "pip", "install", "-U", *requirements_list]
+        )
+    except Exception as e:
+        await message.edit(format_exc(e))
+        db.remove("core.updater", "restart_info")
+    else:
+        await message.edit("<b>Restarting...</b>")
+        restart()
+
+
+modules_help["updater"] = {
+    "update": "Update the userbot. If new core modules are avaliable, they will be installed",
+    "restart": "Restart userbot",
+}
