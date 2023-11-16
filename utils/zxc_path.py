4970cb18202c0cb3787da923109befecb1e64a1b
deadboizxc <deadboi.zxc@gmail.com>
2023-11-16 01:28:04 +0100

    update zxc_path.py | added folder check for Termux/Linux

--- utils/zxc_path.py
+++ utils/zxc_path.py
@@ -5,12 +5,12 @@ from pathlib import Path
 _file_path_ = os.path.abspath(__file__)
 _HOME_DIR_ = os.environ.get('HOME')
 
-_prefix_dir_ = os.environ.get('PREFIX')
-_ROOT_DIR_ = os.path.dirname(_prefix_dir_)
-
+_prefix_dir_= os.path.dirname(os.path.dirname('PREFIX'))
+_ROOT_DIR_ = os.environ.get(_prefix_dir_) if 'PREFIX' in os.environ else '/'
+_BOT_DIR_ = os.path.dirname(os.path.dirname(_file_path_))
 _MAIN_FILE_ = 'main.py'
 
-_BOT_DIR_ = os.path.dirname(os.path.dirname(_file_path_))
 _JSON_DIR_ = str(Path(_BOT_DIR_, 'json')) + '/'
 _YTDLP_DIR_ = str(Path(_BOT_DIR_, 'ytdlp_down')) + '/'
 _DOWNLOADS_ =  str(Path(_BOT_DIR_, 'downloads')) + '/'
+
