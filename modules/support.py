10b59464621d7fb4f192865dc84406ed129ee4d5
deadboizxc <deadboi.zxc@gmail.com>
2023-12-07 23:27:19 +0100

    update support.py

--- modules/support.py
+++ modules/support.py
@@ -1,24 +1,20 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+
 import random
 import datetime
 
-from utils.misc import (
-    modules_help,
-    prefix,
-    userbot_version,
-    python_version,
-    gitrepo,
-)
+from utils.misc import modules_help, prefix, userbot_version, python_version, gitrepo
 
 @Client.on_message(filters.command(["support", "repo"], prefix) & filters.me)
 async def support(_, message: Message):
     devs = ["@deadboizxc"]
     random.shuffle(devs)
 
-    commands_count = float(
-        len([cmd for module in modules_help for cmd in module])
-    )
+    commands_count = float(len([cmd for module in modules_help for cmd in module]))
+
+    remote_url = list(gitrepo.remote().urls)[0]
+    branch_link = f"<a href={remote_url}/tree/{gitrepo.active_branch}>{gitrepo.active_branch}</a>"
 
     await message.edit(
         f"<b>💜 zxc-userbot 💜</b>\n\n"
@@ -27,19 +23,16 @@ async def support(_, message: Message):
         "<b>Modules repository:</b> <a href=https://github.com/deadboizxc/custom_modules>"
         "deadboizxc/custom_modules</a>\n"
         f"<b>Main developers:</b> {', '.join(devs)}\n\n"
-        f"<b>Python version:<b/> {python_version}\n"
+        f"<b>Python version:</b> {python_version}\n"
         f"<b>Modules count:</b> {len(modules_help) / 1}\n"
-        f"<b>Commands count:</b> {commands_count}</b>",
+        f"<b>Commands count:</b> {commands_count}\n"
+        f"<b>Branch:</b> {branch_link}",
         disable_web_page_preview=True,
     )
-
 
 @Client.on_message(filters.command(["version", "ver"], prefix) & filters.me)
 async def version(client: Client, message: Message):
     changelog = ""
-    ub_version = ".".join(userbot_version.split(".")[:2])
-    await message.delete()
-
     remote_url = list(gitrepo.remote().urls)[0]
     commit_time = (
         datetime.datetime.fromtimestamp(gitrepo.head.commit.committed_date)
@@ -47,18 +40,29 @@ async def version(client: Client, message: Message):
         .strftime("%Y-%m-%d %H:%M:%S %Z")
     )
 
-    await message.reply(
-        f"<b>💜 <a href = https://github.com/deadboizxc/zxc-userbot>zxc-userbot</a> 💜</b>\n"
-        f"<b>Version: </b><code>{userbot_version}</code>\n"
-        + (
-            f"<b>Branch:</b> <code><a href={remote_url}/tree/{gitrepo.active_branch}>{gitrepo.active_branch}</a></code>\n"
-            if gitrepo.active_branch != "master"
-            else ""
-        )
-        + f"<b>Commit:</b> <code><a href={remote_url}/commit/{gitrepo.head.commit.hexsha}>"
-        f"{gitrepo.head.commit.hexsha[:7]}</a></code> by <b>{gitrepo.head.commit.author.name}</b>\n"
+    update_version_commit = None
+    for commit in gitrepo.iter_commits():
+        if any(keyword in commit.message.lower() for keyword in ["update version", "update ver", "upd ver"]):
+            update_version_commit = commit
+            break
+
+    if update_version_commit:
+        commit_sha = update_version_commit.hexsha
+        ub_version = f'<a href="{remote_url}/commit/{commit_sha}">{userbot_version}</a>'
+    else:
+        ub_version = userbot_version
+
+    branch_link = f"<a href={remote_url}/tree/{gitrepo.active_branch}>{gitrepo.active_branch}</a>"
+
+    await message.edit(
+        f"<b>💜 <a href=https://github.com/deadboizxc/zxc-userbot>zxc-userbot</a> 💜</b>\n"
+        f"<b>Version:</b> {ub_version}\n"
+        + (f"<b>Branch:</b> {branch_link}\n" if gitrepo.active_branch != "master" else "")
+        + f"<b>Commit:</b> <a href={remote_url}/commit/{gitrepo.head.commit.hexsha}>"
+        f"{gitrepo.head.commit.hexsha[:7]}</a> by <b>{gitrepo.head.commit.author.name}</b>\n"
         f"<b>Commit time:</b> <code>{commit_time}</code>",
-    disable_web_page_preview=True)
+        disable_web_page_preview=True
+    )
 
 
 modules_help["support"] = {
